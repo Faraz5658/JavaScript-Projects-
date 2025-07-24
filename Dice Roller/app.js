e6031075d0adbd74fa82158f47252a2d621603dc
Faraz5658 <farazahmad5623@gmail.com>
2025-07-24 11:23:46 +0500

    Dice Roller

---
+++ Dice Roller/app.js
@@ -0,0 +1,6 @@
+function roll() {
+      let d1 = Math.floor(Math.random() * 6) + 1;
+      let d2 = Math.floor(Math.random() * 6) + 1;
+      document.getElementById("result").innerText = ` ${d1} + ${d2} = ${d1 + d2}`;
+    }
+
