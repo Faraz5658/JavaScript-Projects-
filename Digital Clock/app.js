5ea8738daa1f7bd28d5b97220cd29a8d9564e057
Faraz5658 <farazahmad5623@gmail.com>
2025-07-24 11:21:03 +0500

    Digital Clock

---
+++ Digital Clock/app.js
@@ -0,0 +1,16 @@
+function updateClock() {
+    const now = new Date();
+  
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2 , '0');
+    let seconds = now.getSeconds().toString().padStart(2, '0');
+  
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').textContent = currentTime;
+  }
+  
+  // Update clock every second
+  setInterval(updateClock, 1000);
+  
+  // Initial call
+  updateClock();
